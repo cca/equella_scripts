345867474846dd985202cba9045afd2c624d4049
phette23 <phette23@gmail.com>
2014-09-17 17:37:36 -0700

    script to split out COURSE LIST taxonomy

---
+++ courseInfoNodesSplit.js
@@ -0,0 +1,10 @@
+// this scripts splits the course taxonomy into separate metadata nodes
+//
+// it's very common to ask users to select their section from a COURSE LIST
+// taxonomy structured like so: SEMESTER\COURSE\FACULTY\SECTION
+// this parses that tree out & inserts into pertinent local/courseInfo nodes
+var subjectsplit = xml.get('local/courseInfo/courseinfo').split("\\\\");
+xml.set('local/courseInfo/semester', subjectsplit [0]);
+xml.set('local/courseInfo/course', subjectsplit [1]);
+xml.set('local/courseInfo/faculty', subjectsplit [2]);
+xml.set('local/courseInfo/section', subjectsplit [3]);

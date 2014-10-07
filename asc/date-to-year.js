2cb0864f0c524445f1dcd2a6fdd0ddc4d389fc02
phette23 <phette23@gmail.com>
2014-10-07 14:10:31 -0700

    date-to-year: update again to work around EQUELLA bug

--- asc/date-to-year.js
+++ asc/date-to-year.js
@@ -2,14 +2,17 @@
 // the actual value that's recorded is a full date (e.g. when user selects
 // "2014" the full date "2014-01-01" is value retained). This script
 // accompanies any year-only date pickers; change the dateXPath to target
-// the same metadata node, then put this script inside an Advanced Scripting
-// Control, specifically the On-Submit Script section at the bottom.
-var dateXPath = '/mods/name/subNameWrapper/gradDate',
-    date = xml.get(dateXPath);
+// the same metadata node, then put this script inside the Expert Scripting
+// Save Script.
+//
+// Note that, unfortunately due to a bug in the way EQUELLA reads back dates
+// into the datepicker form UI, you need a staging area to store the (full
+// length) date and then a destination path which will store the year only.
+var date = xml.get('/path/to/staging');
 
-// weird behavior; if EQUELLA doesn't recognize date value it'll set it to 
+// weird behavior; if EQUELLA doesn't recognize date value it'll set it to
 // "2020" (no month, no day), below we work around that
-if (date != '' && date != '2020') {
+if (date != '') {
     var year = date.substr(0,4);
-    xml.set(dateXPath, year);
+    xml.set('/path/to/date', year);
 }

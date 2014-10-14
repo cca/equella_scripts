34af2272ea772408e1cf1e1f099fc0cc8c8021d9
phette23 <phette23@gmail.com>
2014-10-13 19:14:47 -0700

    add JS for autofilling names from user info

---
+++ asc/name-autofill.js
@@ -0,0 +1,12 @@
+// a commonly-used Advanced Scripting Control
+// to fill in mods/name/namePart based on user's name
+// typically put in an On-Load Script
+// & accompanied by a Display Template showing the name
+var firstname = user.getFirstName();
+var lastname = user.getLastName();
+var username = user.getUsername();
+
+var fullname = lastname + ", " + firstname;
+
+xml.set('mods/name/namePart',fullname);
+xml.set('mods/name/subNameWrapper/username',username);

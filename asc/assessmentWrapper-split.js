1e133c3e242a95d84232556c00ee4116230889e4
phette23 <phette23@gmail.com>
2014-10-17 13:56:40 -0700

    assessmentWrapper split - more portable, use condition to interpret meaning of leaf node

--- asc/assessmentWrapper-split.js
+++ asc/assessmentWrapper-split.js
@@ -13,10 +13,20 @@ var assessmemt = xml.get('local/assessmentWrapper/staging').split("\\\\");
 // guard against aW/staging being empty, xml.get returns
 // empty string if node is empty, so assessment.length != 0
 if (assessment[0] != '') {
+    var type = assessment[1];
+
     xml.set('local/assessmentWrapper/date', assessment[0]);
-    xml.set('local/assessmentWrapper/type', assessment[1]);
-    xml.set('local/assessmentWrapper/program', assessment[2]);
-    // two uses for leaf of taxonomy; CCA program or accrediting body
-    // if latter, uncomment the line below & comment out line above
-    // xml.set('local/assessmentWrapper/organization', assessment[2])
+    xml.set('local/assessmentWrapper/type', type);
+
+    // the taxonomy leaf node has 2 distinct meanings:
+    // could be a CCA program for assessment, ext. reviews, etc.
+    // or an accrediting body (NASAD, etc.) for accreditation
+    if (type == 'External Review' || type == 'Assessment') {
+        xml.set('local/assessmentWrapper/program', assessment[2]);
+    } else if (type == 'Accreditation') {
+        xml.set('local/assessmentWrapper/organization', assessment[2]);
+    } else {
+        // @todo is this the sensible fallback?
+        xml.set('local/assessmentWrapper/program', assessment[2]);
+    }
 }

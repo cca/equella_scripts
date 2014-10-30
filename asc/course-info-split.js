28dfb3187cdcf7cda3883d25705ba6e18150787c
phette23 <phette23@gmail.com>
2014-10-30 16:06:34 -0700

    course info splitter - guard against courseinfo node being empty

--- asc/course-info-split.js
+++ asc/course-info-split.js
@@ -4,7 +4,12 @@
 // taxonomy structured like so: SEMESTER\COURSE\FACULTY\SECTION
 // this parses that tree out & inserts into pertinent local/courseInfo nodes
 var subjectsplit = xml.get('local/courseInfo/courseinfo').split("\\\\");
-xml.set('local/courseInfo/semester', subjectsplit [0]);
-xml.set('local/courseInfo/course', subjectsplit [1]);
-xml.set('local/courseInfo/faculty', subjectsplit [2]);
-xml.set('local/courseInfo/section', subjectsplit [3]);
+
+// guard against courseinfo being empty
+// "".split => [""] so we can't test if [].length == 0
+if (subjectsplit[0] != "") {
+    xml.set('local/courseInfo/semester', subjectsplit [0]);
+    xml.set('local/courseInfo/course', subjectsplit [1]);
+    xml.set('local/courseInfo/faculty', subjectsplit [2]);
+    xml.set('local/courseInfo/section', subjectsplit [3]);
+}

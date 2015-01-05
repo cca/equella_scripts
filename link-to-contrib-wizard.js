8751911c2e818e8e984bd4a70c2711afccf83896
phette23 <phette23@gmail.com>
2015-01-05 15:36:03 -0800

    even better: skip prompt middleman & make contrib wiz URL straight from item's XML

--- link-to-contrib-wizard.js
+++ link-to-contrib-wizard.js
@@ -1,9 +1,9 @@
 /* global prompt */
 /*
- * Bookmarklet to take a collection ID and output to URL for
- * its contribution wizard. Run `make contrib-wiz` to create
+ * Bookmarklet to take a collection ID from the XML of one of its items and
+ * output the URL for its contribution wizard. Run `make contrib-wiz` to create
  * it & copy to your clipboard.
  */
-var uuid = prompt('Enter collection ID (/item/@itemdefid in the XML):')
+var uuid = document.getElementsByTagName('item')[0].getAttribute('itemdefid')
     , stem = 'https://vault.cca.edu/access/runwizard.do?method=newitem&itemdefUuid=';
 void prompt('Use ⌘+C & then Return to copy URL to clipboard.', stem + uuid);

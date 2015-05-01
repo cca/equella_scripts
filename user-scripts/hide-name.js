5524c16711c07deaff3d72b01938e3e31e8bedbf
phette23 <phette23@gmail.com>
2015-05-01 15:43:41 -0700

    more detailed script for internal curricular assessment

--- user-scripts/hide-name.js
+++ user-scripts/hide-name.js
@@ -9,8 +9,19 @@ var titleXPath = 'mods/titleInfo/title',
     title = xml.get(titleXPath),
     id = xml.get('mods/recordInfo/recordIdentifier'),
     // newTitle is 1st 8 digits of ID
-    newTitle = id.split('-')[0];
+    newTitle = id.split('-')[0]
 
-// store title elsewhere, then change it
-xml.set('tmp', title);
-xml.set(titleXPath, newTitle);
+// store title elsewhere, then change it. All we need to do to undo is:
+// var tmp = xml.get('tmp')
+// xml.set('mods/titleInfo/title', tmp)
+xml.set('tmp', title)
+xml.set(titleXPath, newTitle)
+// store what we used as the unique ID for later use
+xml.set('mods/identifier', newTitle)
+
+// mark that this item was used in an assessment activity
+var xp = 'local/assessmentWrapper/'
+xml.set(xp + 'date', 'Spring 2015')
+xml.set(xp + 'useInReview', 'yes')
+xml.set(xp + 'type', 'Course Assessment')
+xml.set(xp + 'program', 'Writing & Literature')

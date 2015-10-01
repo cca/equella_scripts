f8ea27a477e52069c75385d448e0902d59458480
phette23 <phette23@gmail.com>
2015-10-01 12:30:37 -0700

    copy contrib link from main item page

--- link-to-contrib-wizard.js
+++ link-to-contrib-wizard.js
@@ -4,6 +4,18 @@
  * output the URL for its contribution wizard. Run `make contrib-wiz` to create
  * it & copy to your clipboard.
  */
-var uuid = document.getElementsByTagName('item')[0].getAttribute('itemdefid')
+var uuid = ''
     , stem = 'https://vault.cca.edu/access/runwizard.do?method=newitem&itemdefUuid=';
+try {
+    // for <XML> pages
+    uuid = document.getElementsByTagName('item')[0]
+            .getAttribute('itemdefid')
+} catch(e) {
+    // get collection ID from first breadcrumb's href
+    // it's in the query string, like "…?in=C${uuid}&…"
+    uuid = document.getElementById('breadcrumb-inner')
+            .getElementsByTagName('a')[0]
+            .getAttribute('href')
+            .match(/\?in=C(.*?)&/)[1]
+}
 void prompt('Use ⌘+C & then Return to copy URL to clipboard.', stem + uuid);

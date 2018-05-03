e5b78e729e4f820fa6fe12675e8cfd9298ca16c3
phette23 <phette23@gmail.com>
2018-05-03 13:30:05 -0700

    use more appropriate name terms

--- asc/name-autofill.js
+++ asc/name-autofill.js
@@ -2,11 +2,11 @@
 // to fill in mods/name/namePart based on user's name
 // typically put in an On-Load Script
 // & accompanied by a Display Template showing the name
-var firstname = user.getFirstName();
-var lastname = user.getLastName();
+var givenname = user.getFirstName();
+var surname = user.getLastName();
 var username = user.getUsername();
 
-var fullname = lastname + ", " + firstname;
+var fullname = surname + ", " + givenname;
 
 xml.set('mods/name/namePart', fullname);
 xml.set('mods/name/subNameWrapper/username', username);

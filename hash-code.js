d72842c3aeb6ac5cb15813bf298cdfd227f0f74c
phette23 <phette23@gmail.com>
2019-07-19 17:08:24 -0700

    use `var`, stop being lazy

--- hash-code.js
+++ hash-code.js
@@ -1,10 +1,10 @@
 // useful for finding location of files on server, location is
 // {{data dir}}/Institutions/cca2012/Attachments/${hashCode(uuid)}/${uuid}/${version}
-hashCode = function(str){
+var hashCode = function(str){
 	var hash = 0;
 	if (str.length == 0) return hash;
-	for (i = 0; i < str.length; i++) {
-		char = str.charCodeAt(i);
+	for (var i = 0; i < str.length; i++) {
+		var char = str.charCodeAt(i);
 		hash = ((hash<<5)-hash)+char;
 		hash = hash & hash; // Convert to 32bit integer
 	}

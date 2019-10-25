b84e19da8332d590137ebce0f962514ec80ae286
phette23 <phette23@gmail.com>
2019-10-25 16:02:51 -0700

    syllabus-download: code cleanup & accept range of semesters
    can specify array of semesters instead of years which makes it easier
    to get exactly the files we want

--- utilities/syllabus-download/index.js
+++ utilities/syllabus-download/index.js
@@ -1,23 +1,36 @@
 /*jshint esversion: 6, node: true */
+const fs = require('fs')
+const path = require('path')
+
 const request = require('request')
 const xpath = require('xpath')
 const xmldom = require('xmldom').DOMParser
 const async = require('async')
-const fs = require('fs')
-const path = require('path')
 
 const token = fs.readFileSync('.token').toString().trim()
-let headers = {
+const headers = {
     'Accept': 'application/json',
     'X-Authorization': 'access_token=' + token,
 }
-const root = 'https://vault.cca.edu/api/'
+const root = 'https://vault.cca.edu/api'
 // parameters for filtering
-let collections = ['9ec74523-e018-4e01-ab4e-be4dd06cdd68'] // ID for Syllabus Coll
-let programs = ['ILLUS']
-let years = [2017, 2018, 2019]
+const collections = ['9ec74523-e018-4e01-ab4e-be4dd06cdd68'] // ID for Syllabus Coll
+const length = 50 // maximum no. of items we can get in an API request
+const programs = ['TEXTL']
+// the utilities/semesters.js script can generate this list easily
+const semesters = [
+    "Fall 2019",
+    "Summer 2019",
+    "Spring 2019",
+    "Fall 2018",
+    "Summer 2018",
+    "Spring 2018",
+    "Fall 2017",
+    "Summer 2017",
+    "Spring 2017",
+    "Fall 2016",
+]
 
-let length = 50 // maximum no. of items we can get in an API request
 // combine multiple programs into one XPath-like "where" clause
 let where_clause = ''
 for (var i = 0; i < programs.length; i++) {
@@ -36,43 +49,36 @@ let params = {
 
 let baseRequest = request.defaults({ headers: headers, json: true })
 let searchRequest = baseRequest.defaults({ qs: params })
+
 let total = 0
 let count = 0
 let items = []
+// collect items from a request into global array, set total in case it's changed
+function collectItems (err, resp, data) {
+    if (err) {
+        console.error(`Error getting syllabi search results. Count was ${items.length}.`)
+        throw err
+    }
+    items = items.concat(data.results)
+    total = data.available
+}
 
 // figure out how many syllabi there are & collect them into items array
-searchRequest.get(root + 'search/', (err, response, data) => {
-    if (err) {
-        console.log(`Error with initial syllabus search.`)
-        console.log(err)
-    }
+searchRequest.get(`${root}/search/`, (err, response, data) => {
+    if (err) throw err
 
-    total = data.available
-    items = items.concat(data.results)
-    count += items.length
+    collectItems(null, null, data)
     console.log(`${total} total syllabi for ${programs.join(', ')}`)
-    // handle scenario where there are <50 syllabi & we got them all already
-    if (count === total) downloadFiles()
-
     // these requests will fire off in parallel
     while (count < total) {
         console.log(`Getting syllabi items ${count + 1} through ${count + length}...`)
-        searchRequest.get(root + 'search/', { qs: { start: count } },
-            (err, response, data) => {
-                if (err) {
-                    console.log(`Error getting syllabi search results. Count was ${count}.`)
-                    console.log(err)
-                }
-                items = items.concat(data.results)
-                // we have all the items so can start downloading
-                // use > because it's possible an item was created in the meantime
-                if (items.length >= total) downloadFiles()
-        })
+        searchRequest.get(`${root}/search/`, { qs: { start: items.length } }, collectItems)
         count += length
+        if (count >= total) downloadFiles(items)
     }
 })
 
-function downloadFiles () {
+function downloadFiles (items) {
     console.log('Downloading files (this may take a minute).')
     async.each(items, getFile)
 }
@@ -80,17 +86,16 @@ function downloadFiles () {
 function getFile (item, callback) {
     let xml = new xmldom().parseFromString(item.metadata)
     let semester = xpath.select('string(//local/courseInfo/semester)', xml)
-    let year = parseInt(semester.match(/\d{4}/)[0])
-    let section = [
-        xpath.select('string(//local/courseInfo/semester)', xml),
-        xpath.select('string(//local/courseInfo/section)', xml),
-        xpath.select('string(//local/courseInfo/course)', xml),
-    ].join(' ')
 
-    if (year && years.includes(year)) {
+    if (semester && semesters.includes(semester)) {
+        let section = [
+            semester,
+            xpath.select('string(//local/courseInfo/section)', xml),
+            xpath.select('string(//local/courseInfo/course)', xml),
+        ].join(' ')
         // handle multiple attachments, add number to filename
         item.attachments.forEach((attachment, index) => {
-            let req = baseRequest.get(`${root}item/${item.uuid}/${item.version}/file/${attachment.filename}`)
+            let req = baseRequest.get(`${root}/item/${item.uuid}/${item.version}/file/${attachment.filename}`)
                 .on('response', res => {
                     let extension = attachment.filename.split('.').pop()
                     let filename = [section, (index ? ` (${index})` : ''), '.', extension].join('')
@@ -98,8 +103,8 @@ function getFile (item, callback) {
                     req.pipe(fs.createWriteStream(path.join('files', filename)))
                 })
                 .on('err', err => {
-                    console.log(`Error downloading file ${attachment.filename} from item https://vault.cca.edu/items/${item.uuid}/${item.version}/`)
-                    console.log(err)
+                    console.error(`Error downloading file ${attachment.filename} from item https://vault.cca.edu/items/${item.uuid}/${item.version}/`)
+                    console.error(err)
                 })
         })
     }
--- utilities/syllabus-download/readme.md
+++ utilities/syllabus-download/readme.md
@@ -1,13 +1,13 @@
 # Bulk Download Syllabus Files
 
-The purpose of this script is to provide a way to easily supply a department with copies of all their syllabi in VAULT, e.g. so they can be included in an external review or other assessment activity. It downloads all the syllabi items in VAULT for one or more programs, finds the sections within a provided range of years, and saves their attachments in the "files" directory with filenames of form "Spring 2017 ILLUS-230-01 Tools_ Hand-Lettering.pdf" e.g. semester, followed by section code, followed by course name.
+The purpose of this script is to provide a way to easily supply a department with copies of all their syllabi in VAULT, e.g. so they can be included in an external review or other assessment activity. It downloads all the syllabi items in VAULT for one or more programs, finds the sections within a provided range of academic terms, and saves their attachments in the "files" directory with filenames of form "Spring 2017 ILLUS-230-01 Tools_ Hand-Lettering.pdf" e.g. semester, followed by section code, followed by course name.
 
 To run the script:
 
 - ensure you have node & npm
 - run `npm install` to get dependencies
 - obtain an API access token from VAULT & put it in a text file named ".token" in this folder
-- edit index.js (see the `programs` and `years` variables towards the top)
+- edit index.js (see the `programs` and `semesters` variables towards the top)
 - run `node index` to download the files
 
 If running repeated times, you may want to clean out the files directory in between. The script is meant to be flexible enough to extract multiple years of syllabi from multiple departments. To get _all_ syllabi, you could simply remove the `where` parameter from the initial API requests.

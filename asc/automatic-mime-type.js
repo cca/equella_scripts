4fc2607298af8ff11e8fc978bcc7d8d130171870
phette23 <phette23@gmail.com>
2020-02-12 09:44:44 -0800

    asc - auto-mime type should use built-in oE mime object

--- asc/automatic-mime-type.js
+++ asc/automatic-mime-type.js
@@ -1,28 +1,11 @@
 /* global attachments,xml */
-/* an on-submit script to record the MIME types of attachments
-in an item's metadata. Based purely off of filename extension, not
-file's bytes. Incomplete listing of MIME types. */
+/* use oE's mime object to record the MIME types of attachments
+used in Libraries collection Expert Save Script */
 // returns iterator, not array
 var files = attachments.list()
 var len = files.size()
 // MODS MIME type element
 var xp = '/mods/physicalDescription/internetMediaType'
-// hash of file extensions to corresponding MIME types
-// complete list: https://www.iana.org/assignments/media-types/media-types.xhtml
-var mimes = {
-    'doc': 'application/msword',
-    'gif': 'image/gif',
-    'jpg': 'image/jpeg',
-    'jpeg': 'image/jpeg',
-    'mp3': 'audio/mpeg3 ',
-    'mp4': 'video/mp4',
-    'mov': 'video/quicktime',
-    'png': 'image/png',
-    'pdf': 'application/pdf',
-    'tif': 'image/tiff',
-    'tiff': 'image/tiff',
-    'wav': 'audio/wav'
-}
 
 // delete all previous MIME type info
 xml.deleteAll(xp)
@@ -30,10 +13,12 @@ xml.deleteAll(xp)
 for (var i = 0; i < len; i++) {
     var file = files.get(i)
     var filename = String(file.getFilename())
-    var ext = filename.split('.').pop().toLowerCase()
+    // https://openequella.github.io/api-docs/Script/api/com/tle/web/scripting/advanced/objects/MimeScriptObject.html
+    // returns "null if the mime type could not be determined"
+    var mimeObject = mime.getMimeTypeForFilename(filename)
 
-    // do we have a MIME type for this file extension?
-    if (mimes[ext]) {
-        xml.add(xp, mimes[ext])
+    if (mimeObject) {
+        // https://openequella.github.io/api-docs/Script/api/com/tle/web/scripting/advanced/types/MimeTypeScriptType.html
+        xml.add(xp, mimeObject.getType())
     }
 }

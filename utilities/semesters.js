70a7aaa6e78779915fee823e0ad18c80446fb971
phette23 <phette23@gmail.com>
2020-05-26 13:36:53 -0700

    semesters.js - modernize JS, explicit how to in comment

--- utilities/semesters.js
+++ utilities/semesters.js
@@ -1,21 +1,22 @@
 /*jshint -W083 */
 // usage:
-// node semesters > taxo.csv
-// (creates semester taxonomy from 2003 Spring to 2017 Fall)
-// OR
-// > node semesters 2010 2003 > taxo.csv
-// (creates semester taxonomy from 2010 Spring to 2003 Fall)
-// can then upload with
+// > node semesters STARTYEAR ENDYEAR > taxo.csv
+// creates semester taxonomy from Spring of start year to Fall of end year
+//
+// can then upload to VAULT with
 // > uptaxo --un $USERNAME --pw $PASSWORD --clear --csv taxo.csv --tid $TID
 // and our TID for Semesters taxo is 48af53bc-838a-4e5b-985c-8dbf5cbe2e45
+//
+// this is the complete process:
+// node utilities/semesters.js 2003 2030 && uptaxo --un=(jq -r .username ~/.equellarc) --pw=(jq -r .password ~/.equellarc) --clear --csv taxo.csv --tid 48af53bc-838a-4e5b-985c-8dbf5cbe2e45
 
-/*jshint node:true */
-var start = process.argv[2] || 2003
-var end = process.argv[3] || 2017
-var seasons = ['Fall', 'Summer', 'Spring']
+/*jshint node:true, esversion:6 */
+const start = process.argv[2] || 2003
+const end = process.argv[3] || 2017
+const seasons = ['Fall', 'Summer', 'Spring']
 
-for (var i = end; i > start - 1; i--) {
-    seasons.forEach(function(season) {
+for (let i = end; i > start - 1; i--) {
+    seasons.forEach((season) => {
         console.log(['"', season, ' ', i, '"'].join(''));
     })
 }

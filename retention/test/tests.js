73ef26279c58d3ac389f75e9cb6a22be69a91ff0
Eric Phetteplace <phette23@gmail.com>
2021-07-30 09:43:57 -0700

    retention - finish test suite

--- retention/test/tests.js
+++ retention/test/tests.js
@@ -8,46 +8,58 @@ const defaults = {
 const options = require('rc')('retention', defaults)
 const Item = require('../item.js')
 const items = {
-    award: require('./fixtures/award.json'),
-    excluded: require('./fixtures/excluded-collection.json'),
-    highRated: require('./fixtures/high-rating.json'),
-    old: require('./fixtures/old-item.json'),
-    recent: require('./fixtures/recent-item.json'),
-    untitled: require('./fixtures/untitled.json'),
+    award: new Item(require('./fixtures/award.json'), options),
+    commaInTitle: new Item(require('./fixtures/comma-in-title.json'), options),
+    excluded: new Item(require('./fixtures/excluded-collection.json'), options),
+    highRated: new Item(require('./fixtures/high-rating.json'), options),
+    old: new Item(require('./fixtures/old-item.json'), options),
+    recent: new Item(require('./fixtures/recent-item.json'), options),
+    recentAndExcluded: new Item(require('./fixtures/recent-and-excluded.json'), options),
+    untitled: new Item(require('./fixtures/untitled.json'), options),
 }
 
 describe('Item', () => {
     it('should mark for removal items that are old enough', () => {
-        const old = new Item(items.old, options)
-        const recent = new Item(items.recent, options)
-        assert.equal(old.isOldEnough, true)
-        assert.equal(old.toBeRemoved, true)
-        assert.equal(recent.isOldEnough, false)
-        assert.equal(recent.toBeRemoved, false)
+        assert.equal(items.old.isOldEnough, true)
+        assert.equal(items.old.toBeRemoved, true)
+        assert.equal(items.recent.isOldEnough, false)
+        assert.equal(items.recent.toBeRemoved, false)
     })
 
     it('should not remove items in excluded collections', () => {
-        const excluded = new Item(items.excluded, options)
-        assert.equal(excluded.isntInExcludedCollection, false)
-        assert.equal(excluded.toBeRemoved, false)
+        assert.equal(items.excluded.isntInExcludedCollection, false)
+        assert.equal(items.excluded.toBeRemoved, false)
     })
 
     it('should not remove items with rating = "high"', () => {
-        const highRated = new Item(items.highRated, options)
-        assert.equal(highRated.toBeRemoved, false)
+        assert.equal(items.highRated.isntHighRated, false)
+        assert.equal(items.highRated.toBeRemoved, false)
     })
 
     it('should not remove items that have won awards', () => {
-        const award = new Item(items.award, options)
-        assert.equal(award.toBeRemoved, false)
+        assert.equal(items.award.hasNoAwards, false)
+        assert.equal(items.award.toBeRemoved, false)
     })
 
     it('should be able to handle items with & without titles', () => {
-        const untitled = new Item(items.untitled, options)
-        const old = new Item(items.old, options)
-        assert.equal(untitled.title, '')
-        assert.ok(old.title)
+        assert.equal(items.untitled.title, '')
+        assert.ok(items.old.title)
+    })
+
+    it('should make a list of reasons why item is to be retained', () => {
+        assert.deepEqual(items.recent.reasonsRetained, ['isOldEnough'])
+        // test an item that's retained for multiple reasons
+        assert.ok(items.recentAndExcluded.reasonsRetained.includes('isOldEnough'))
+        assert.ok(items.recentAndExcluded.reasonsRetained.includes('isntInExcludedCollection'))
+    })
+
+    it('should serialize to a CSV string', () => {
+        const csv = items.commaInTitle.toCSV()
+        assert.ok(csv)
+        assert.ok(Item.CSVHeaderRow)
+        // quotes a field that has commas in it
+        assert.ok(csv.match(`"${items.commaInTitle.title}"`))
+        // ends in a newline
+        assert.equal(csv.match(/\n$/)[0], '\n')
     })
 })
-
-// @TODO CSV serializer, reasons retained list

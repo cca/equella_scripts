8aa8be1f9752ff9102a7c410a5c20eebecea3d51
phette23 <phette23@gmail.com>
2022-03-01 15:48:20 -0800

    fix FAJR ASC
    to support two name formats we actually need two for-loops

--- asc/fajr-student-taxo.js
+++ asc/fajr-student-taxo.js
@@ -11,7 +11,8 @@ var nameTaxo = data.getTaxonomyByUuid('b48fa05e-f36c-4fb5-8096-0daa7937be9a')
 var fullname = user.getLastName() + ', ' + user.getFirstName()
 var writtenname = user.getFirstName() + ' ' + user.getLastName()
 // searchTerms returns an iterator, not an array
-var terms = nameTaxo.searchTerms(fullname)
+var terms1 = nameTaxo.searchTerms(fullname)
+var terms2 = nameTaxo.searchTerms(writtenname)
 
 function set(path, value) {
     if (value) {
@@ -19,30 +20,52 @@ function set(path, value) {
     }
 }
 
+function writeMetadata(term) {
+    var username = term.getData('username')
+    var major = term.getData('major')
+    var semester = term.getData('semester')
+    var studentID = term.getData('studentID')
+    // accidentally loaded studentID under "id" key during 2020FA
+    if (!studentID) studentID = term.getData('id')
+
+    set('mods/name/subNameWrapper/username', username)
+    set('mods/name/subNameWrapper/major', major)
+    // also set the department value, used in All Program Work hierarchy
+    set('local/department', major)
+    set('local/courseInfo/semester', semester)
+    set('mods/name/subNameWrapper/ccaAffiliated', studentID)
+}
+
 // note: searchTerms searches full term paths for query string _with wildcard
 // appended_ (e.g. John Smith also returns John Smithsonian)
 // below, we ensure the term we use actually matches the name we want
-for (var i = terms.size() - 1; i >= 0; i--) {
-    var term = terms.get(i)
+// We support both forms of names, "Surname, Givenname" and "Givenname Surname"
+// via these two loops over different sets of search results.
+var foundStudent = false
+for (var i = terms1.size() - 1; i >= 0; i--) {
+    var term = terms1.get(i)
 
-    // support both forms of names, "Surname, Givenname" and "Givenname Surname"
-    if (term.getTerm() == fullname || term.getTerm() == writtenname) {
-        var username = term.getData('username')
-        var major = term.getData('major')
-        var semester = term.getData('semester')
-        var studentID = term.getData('studentID')
-        // accidentally loaded studentID under "id" key during 2020FA
-        if (!studentID) studentID = term.getData('id')
+
+    if (term.getTerm() == fullname) {
+        writeMetadata(term)
+        foundStudent = true
         break;
     }
 }
 
-set('mods/name/subNameWrapper/username', username)
-set('mods/name/subNameWrapper/major', major)
-// also set the department value, used in All Program Work hierarchy
-set('local/department', major)
-set('local/courseInfo/semester', semester)
-set('mods/name/subNameWrapper/ccaAffiliated', studentID)
+for (var i = terms2.size() - 1; i >= 0; i--) {
+    var term = terms2.get(i)
+
+    if (term.getTerm() == writtenname) {
+        writeMetadata(term)
+        foundStudent = true
+        break;
+    }
+}
+
+if (!foundStudent) {
+    set('debug', 'Unable to find a matching term in the Fine Arts Junior Review Students taxonomy for this user.')
+}
 
 var xp = 'local/viewLevel'
 if (!xml.exists(xp)) xml.set(xp, 'for internal Fine Arts Division use only')

bee2cb32def1e072191c5f1ddad8dac6e9aa6351
phette23 <phette23@gmail.com>
2022-04-11 16:05:17 -0700

    retention - script to count worktype values of items file

---
+++ retention/worktype-count.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+const xmldom = require('@xmldom/xmldom').DOMParser
+const xpath = require('xpath')
+
+const file = process.argv[2]
+if (!file) {
+    console.error("Error: must provide a JSON file of items as the only argument.")
+    process.exit(1)
+}
+
+const items = JSON.parse(fs.readFileSync(file))
+const types = [ "workType", "courseWorkType", "courseWorkTypeSpecific"]
+let counts = {}
+types.forEach(t => counts[t] = { "(null)": 0 })
+
+items.forEach(item => {
+    let xml = new xmldom().parseFromString(item.metadata)
+
+    types.forEach(type => {
+        let value = xpath.select1(`string(//local/courseWorkWrapper/${type})`, xml)
+        if (value) {
+            if (!counts[type][value]) return counts[type][value] = 1
+            return counts[type][value]++
+        }
+        return counts[type]["(null)"]++
+    })
+})
+
+console.log(JSON.stringify(counts, null, 2))

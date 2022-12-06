b86359d76fee2cf1aa77474d356d14baaa25ebaf
phette23 <phette23@gmail.com>
2022-12-06 08:54:13 -0800

    modernize hash-code script, ESM 'main' check
    ref #23

--- utilities/hash-code.js
+++ utilities/hash-code.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
+import {pathToFileURL} from 'node:url'
 // useful for finding location of files on server, location is
 // {{data dir}}/Institutions/cca2012/Attachments/${hashCode(uuid)}/${uuid}/${version}
-var hashCode = function(str){
-    var hash = 0;
-    if (str.length == 0) return hash;
-    for (var i = 0; i < str.length; i++) {
-        var char = str.charCodeAt(i);
-        hash = ((hash<<5)-hash)+char;
-        hash = hash & hash; // Convert to 32bit integer
+function hashCode (str){
+    let hash = 0
+    if (str.length == 0) return hash
+    for (let i = 0; i < str.length; i++) {
+        let char = str.charCodeAt(i)
+        hash = ((hash<<5)-hash)+char
+        hash = hash & hash // Convert to 32bit integer
     }
-    return hash & 127;
+    return hash & 127
 }
 
-if (require && require.main == module) {
-    var uuid = process.argv[2]
+if (import.meta.url === pathToFileURL(process.argv[1]).href) {
+    const uuid = process.argv[2]
     console.log(hashCode(uuid))
 }

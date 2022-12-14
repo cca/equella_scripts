0da9680e3180d1b3556376dda8b0f87893f22d42
phette23 <phette23@gmail.com>
2022-12-14 15:46:41 -0800

    convert contribution-count utility to node-fetch, ref #26

--- utilities/contribution-count/index.js
+++ utilities/contribution-count/index.js
@@ -1,47 +1,45 @@
 // you can pass any valid parameter for the search API route on the CLI
 // https://vault.cca.edu/apidocs.do#!/search/searchItems_get_0
 // e.g. node index --order=relevance --count=500 --status=draft --modifiedAfter=2019-09-01
-import qs from 'qs'
-
 import rc from 'rc'
-import request from 'request'
+import { default as fetch, Headers } from 'node-fetch'
 
-let defaults = { length: 50, count: 200, "q": "", order: "modified" }
+const defaults = { length: 50, count: 200, "q": "", order: "modified" }
 let options = rc('contribution-count', defaults)
 
-let headers = {
+const headers = new Headers({
     'X-Authorization': 'access_token=' + options.token,
     'Content-Type': 'application/json',
-}
+})
 let items = []
 // log messages only when debug=true
-let debug = (msg) => {
-    if (options.debug) console.error(msg)
+function debug () {
+    if (options.debug) console.error(...arguments)
 }
 
 function getItems(start=0) {
     if (items.length < options.count) {
         debug(`Getting items ${items.length + 1} through ${items.length + options.length}...`)
-        let reqOptions = {
-            headers: headers,
-            url: `${options.root}/search?start=${start}&info=detail&${qs.stringify(options)}`,
-            json: true
-        }
-        request(reqOptions, (err, resp, data) => {
-            if (err) {
-                throw err
+        const params = new URLSearchParams(options)
+        const url = `${options.root}/search?start=${start}&info=detail&${params.toString()}`
+
+        fetch(url, {headers: headers}).then(r => r.json())
+            .then(data => {
                 // API sends a { code, error, error_description } error response
-            } else if (data.error) {
-                console.error('EQUELLA API Error:', data)
+                if (data.error) {
+                    throw new Error(`EQUELLA API Error: ${JSON.stringify(data)}`)
+                }
+                // the first time through, if our count is higher than the total
+                // of available items, reset the count to be that total
+                if (start === 0 && data.available < options.count) options.count = data.available
+                items = items.concat(data.results)
+                // recursively call this function until we're done
+                return getItems(items.length)
+            })
+            .catch(e => {
+                console.error(e)
                 process.exit(1)
-            }
-            // the first time through, if our count is higher than the total
-            // of available items, reset the count to be that total
-            if (start === 0 && data.available < options.count) options.count = data.available
-            items = items.concat(data.results)
-            // recursively call this function until we're done
-            return getItems(items.length)
-        })
+            })
     } else {
         collectionCount(items)
     }
@@ -53,36 +51,36 @@ function collectionCount(items) {
     // will start out as a { uuid: 1 } hash & end up as
     // { uuid: { count: 1, name: Libraries } }
     let counts = {}
-    items.map(i => i.collection.uuid).forEach(c => {
+    items.map(i => i.collection.uuid).forEach(uuid => {
         // collection doesn't exist in hash yet, initialize at 0
-        if (!counts[c]) counts[c] = 0
-        return ++counts[c]
+        if (!counts[uuid]) counts[uuid] = 0
+        return ++counts[uuid]
     })
 
     // convert collection UUIDs to names
     debug('Getting names for collection UUIDs...')
-    let reqOptions = {
-        headers: headers,
-        url: `${options.root}/collection/?privilege=VIEW_ITEM&length=500`,
-        json: true
-    }
+    const url = `${options.root}/collection/?privilege=VIEW_ITEM&length=500`
+
     let uuids = Object.keys(counts)
-    request(reqOptions, (err, resp, data) => {
-        if (err) throw err
-        uuids.forEach(uuid => {
-            counts[uuid] = {
-                count: counts[uuid],
-                name: data.results.find(coll => coll.uuid === uuid).name
-            }
+    fetch(url, {headers: headers}).then(r => r.json())
+        .then(data => {
+            uuids.forEach(uuid => {
+                counts[uuid] = {
+                    count: counts[uuid],
+                    name: data.results.find(coll => coll.uuid === uuid).name
+                }
+            })
+            // print "sorted hash" as best we can in JS
+            console.log(`${'Collection'.padEnd(41)}\tContribution count`)
+            uuids.sort((a, b) => {
+                return counts[b].count - counts[a].count
+            }).forEach(uuid => {
+                console.log(`${counts[uuid].name.padEnd(40)} \t${counts[uuid].count}`);
+            });
+        }).catch(e => {
+            console.error(e)
+            process.exit(1)
         })
-        // print "sorted hash" as best we can in JS
-        console.log(`${'Collection'.padEnd(41)}\tContribution count`)
-        uuids.sort((a, b) => {
-            return counts[b].count - counts[a].count
-        }).forEach(uuid => {
-            console.log(`${counts[uuid].name.padEnd(40)} \t${counts[uuid].count}`);
-        });
-    })
 }
 
 getItems()

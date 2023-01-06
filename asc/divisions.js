5b5db70c2e1b861d41aa95b40bd9e85e4bd823d9
phette23 <phette23@gmail.com>
2023-01-06 15:28:19 -0800

    GAMES is a degree program now

--- asc/divisions.js
+++ asc/divisions.js
@@ -51,7 +51,6 @@ var map = {
     'FINAR': ['Fine Arts (MFA)', 'Fine Arts'],
     'FURNT': ['Furniture (BFA)', 'Design'],
     'FYCST': ['First Year Program', 'Humanities & Sciences'],
-    // new as of 2021SP @TODO is this a degree-seeking program?
     'GAMES': ['Game Arts Program', 'Fine Arts'],
     'GLASS': ['Glass (BFA)', 'Fine Arts'],
     'GRAPH': ['Graphic Design (BFA)', 'Design'],

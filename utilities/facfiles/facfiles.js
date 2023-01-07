079a6e8b0d64bc1052181a9ed266047b7a47bfaf
phette23 <phette23@gmail.com>
2023-01-06 16:44:16 -0800

    facfiles - migrate to node-fetch ref #26, small refactoring

--- utilities/facfiles/facfiles.js
+++ utilities/facfiles/facfiles.js
@@ -2,77 +2,68 @@ import fs  from 'node:fs'
 import path  from 'node:path'
 
 import async  from 'async'
-// @TODO switch to node-fetch
-import request  from 'request'
+import {default as fetch, Headers} from 'node-fetch'
 import rc from 'rc'
 import { DOMParser as xmldom } from '@xmldom/xmldom'
 import xpath  from 'xpath'
 
 const options = rc('facfiles')
-
 const LENGTH = 50
-
-const headers = {
+const headers = new Headers({
     'Accept': 'application/json',
     'X-Authorization': 'access_token=' + options.token,
-}
-
+})
 // query string parameters for API, see apidocs.do
-let params = {
+const params = new URLSearchParams({
     collections: options.collection_uuid,
     info: 'metadata,attachment',
     // maximum no. of items we can get in an API request
     length: LENGTH,
     q: options.name
-}
-
-let baseRequest = request.defaults({ headers: headers, json: true })
-let searchRequest = baseRequest.defaults({ qs: params })
+})
 
 let total = 0
-let count = 0
+let count = LENGTH
 let items = []
 
 // determine if a semester falls within the start/stop term parameters
 function inTimeRange(semester) {
-    let num = termToNumber(semester)
+    const num = termToNumber(semester)
     return (num >= start && num <= stop)
 }
 // map semester to number e.g. Fall 2020 => 2020.8, Spring 2019 => 2019.2
 function termToNumber(semester) {
     const map = { 'spring': 0.2, 'summer': 0.5, 'fall': 0.8 }
-    let year = parseInt(semester.match(/\d{4}/)[0])
-    let season = semester.match(/([a-z]+) /i)[1].toLowerCase()
+    const year = parseInt(semester.match(/\d{4}/)[0])
+    const season = semester.match(/([a-z]+) /i)[1].toLowerCase()
     return year + map[season]
 }
 // cache values of these rather than lookup every time
-let start = options.start_term ? termToNumber(options.start_term) : 0
-let stop = options.stop_term ? termToNumber(options.stop_term) : Infinity
+const start = options.start_term ? termToNumber(options.start_term) : 0
+const stop = options.stop_term ? termToNumber(options.stop_term) : Infinity
 
 // collect items from a request into global array, set total in case it's changed
-function collectItems (err, resp, data) {
-    if (err) {
-        console.error(`Error getting syllabi search results. Count was ${items.length}.`)
-        throw err
-    }
+function collectItems (data) {
     items = items.concat(data.results)
     total = data.available
+    console.log(`We have ${items.length} of ${total} total search results for ${options.name}`)
     if (count >= total) downloadFiles(items)
 }
 
-// figure out how many syllabi there are & collect them into items array
-searchRequest.get(`${options.url}/api/search/`, (err, response, data) => {
-    if (err) throw err
-
-    collectItems(null, null, data)
-    console.log(`${total} syllabi search results for ${options.name}`)
-    // these requests fire off in parallel
-    while (count < total) {
-        console.log(`Getting syllabi items ${count + 1} through ${total < count + LENGTH ? total : count + LENGTH}...`)
-        count += LENGTH
-        searchRequest.get(`${options.url}/api/search/`, { qs: { start: items.length } }, collectItems)
-    }
-})
+// figure out how many syllabi there are & collect them into the items array
+function search(start=0) {
+    console.log('searched', start, count, total, items.length)
+    fetch(`${options.root}/api/search/?start=${start}&${params.toString()}`, { headers: headers })
+        .then(r => r.json())
+        .then(data => {
+            collectItems(data)
+            // these requests fire off in parallel
+            while (count < total) {
+                count += LENGTH
+                search(items.length)
+            }
+        })
+}
 
 function downloadFiles (items) {
     console.log('Downloading files (this may take a minute).')
@@ -80,32 +71,33 @@ function downloadFiles (items) {
 }
 
 function getFile (item, callback) {
-    let xml = new xmldom().parseFromString(item.metadata)
-    let semester = xpath.select('string(//local/courseInfo/semester)', xml)
-    let faculty = xpath.select('string(//local/courseInfo/faculty)', xml)
+    const xml = new xmldom().parseFromString(item.metadata)
+    const semester = xpath.select('string(//local/courseInfo/semester)', xml)
+    const faculty = xpath.select('string(//local/courseInfo/faculty)', xml)
 
     if (inTimeRange(semester) && !faculty.indexOf(options.name)) {
-        let section = [
+        const section = [
             semester,
             xpath.select('string(//local/courseInfo/section)', xml),
             xpath.select('string(//local/courseInfo/course)', xml),
         ].join(' ')
         // handle multiple attachments, add number to filename
         item.attachments.forEach((attachment, index) => {
-            let req = baseRequest.get(`${options.url}/api/item/${item.uuid}/${item.version}/file/${encodeURIComponent(attachment.filename)}`)
-                .on('response', res => {
-                    if (res.statusCode != 200) {
-                        return console.error(`${res.statusCode} ERROR: unable to retrieve attachment with filename "${attachment.filename}" for item ${options.url}/items/${item.uuid}/${item.version}/`)
+            fetch(`${options.root}/api/item/${item.uuid}/${item.version}/file/${encodeURIComponent(attachment.filename)}`, { headers: headers })
+                .then(resp => {
+                    if (resp.status != 200) {
+                        return console.error(`${resp.status} ${resp.statusText} ERROR: unable to retrieve attachment "${attachment.filename}" for item ${item.links.view}`)
                     }
-                    let extension = attachment.filename.split('.').pop()
+
+                    const extension = attachment.filename.split('.').pop()
                     let filename = [section, (index ? ` (${index})` : ''), '.', extension].join('')
                     filename = filename.replace(/[/\\:]/g, "_")
-                    req.pipe(fs.createWriteStream(path.join('files', filename)))
-                })
-                .on('error', err => {
-                    console.error(`Error downloading file ${attachment.filename} from item ${options.url}/items/${item.uuid}/${item.version}/`)
-                    console.error(err)
+                    resp.body.pipe(fs.createWriteStream(path.join('files', filename)))
+
+                    resp.body.on('err', e => console.error(e))
                 })
         })
     }
 }
+
+search()

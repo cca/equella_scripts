3395226a5b7594f607da13b629b64f7315ff15f4
phette23 <phette23@gmail.com>
2023-10-03 09:29:12 -0700

    fajr: don't rely on password in .equellarc file
    use 1password cli instead

--- fine-arts-jr-review/fajr/upload.sh
+++ fine-arts-jr-review/fajr/upload.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # sort the taxonomy, just helps when reading through it in admin console
 sort taxo.csv > tmp; mv tmp taxo.csv
+# get password from 1Password
+UN="$(jq -r .username ~/.equellarc)"
+PW="$(op item get "VAULT ($UN)" --fields password || jq -r '.password' ~/.equellarc)"
 # upload it
-python2 uptaxo --un "$(jq -r .username ~/.equellarc)" --pw "$(jq -r .password ~/.equellarc)" --csv taxo.csv --tid "$(eq tax --name 'Fine Arts Junior Review students' | jq -r .uuid)" --start 2
+python2 uptaxo --un $UN --pw $PW --csv taxo.csv --tid "$(eq tax --name 'Fine Arts Junior Review students' | jq -r .uuid)" --start 2
 
 # give option to archive CSVs in data dir
 echo -n 'Archive fajr.csv & taxo.csv in the "data" directory? (y/n) '

b3f7e6832a510b2c7930c9ee5e63444adfe9b319
phette23 <phette23@gmail.com>
2024-04-10 11:29:52 -0700

    chore: update deps, eslint@9
    convert to new eslint flat config file format
    add globals package for use in the eslint config

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import globals from 'globals'
+import js from "@eslint/js"
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                currentItem: 'readonly',
+                data: 'readonly',
+                GmailApp: 'readonly',
+                item: 'readonly',
+                jQuery: 'readonly',
+                $: 'readonly',
+                logger: 'readonly',
+                user: 'readonly',
+                xml: 'readonly'
+            },
+            sourceType: 'module'
+        },
+        rules: {
+            indent: [ 'warn', 4 ],
+            'linebreak-style': [ 'error', 'unix' ],
+            'no-unused-vars': 'off',
+            'no-empty': 'off'
+        }
+    }
+]

54274ac62047075db41e47015c2f310b644a0afc
phette23 <phette23@gmail.com>
2024-05-28 16:08:03 -0700

    feat: facfiles list-download accepts start_term argument

--- utilities/facfiles/list-download.fish
+++ utilities/facfiles/list-download.fish
@@ -2,12 +2,18 @@
 # iterate over an input file of faculty names
 set file $argv[1]
 [ -z $file ] && set file faculty.txt
+set start_term $argv[2]
+if [ -z $start_term ]
+    set start_term ""
+else
+    set start_term "--start_term='$start_term'"
+end
 mkdir -p faculty
 
 for name in (cat $file)
     echo (date) "Downloading syllabi for $name"
     # download all available syllabi
-    node facfiles.js --name "$name"
+    node facfiles.js --name "$name" $start_term
     # put them into their own directory
     set dirname (echo $name | tr -d '\\/()[]{}*?!')
     mkdir -p "faculty/$dirname"

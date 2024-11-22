4f1300ae10a78b6d73a430112335845d01c947e1
phette23 <phette23@gmail.com>
2024-11-22 09:23:28 -0800

    fix: better ingest path validation

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -1,5 +1,5 @@
 import csv
-import os
+from pathlib import Path
 
 import click
 
@@ -7,28 +7,44 @@ import click
 @click.command("ingest")
 @click.help_option("-h", "--help")
 @click.version_option("0.1.0")
-@click.argument("csvfile", type=click.File("r"), metavar="input.csv")
+@click.argument(
+    "csvfile",
+    metavar="input.csv",
+    type=click.Path(exists=True, readable=True, dir_okay=False, path_type=Path),
+)
 @click.option("-v", "--verbose", is_flag=True, help="Print diagnostic messages.")
-def main(csvfile, verbose=False):
-    reader = csv.DictReader(csvfile)
-    for index, row in enumerate(reader):
-        if verbose:
-            click.echo(f"Processing row no. {index + 1}")
-        failure = False
+def main(csvfile: Path, verbose=False):
+    root_dir = csvfile.parent.resolve()
+    if verbose:
+        click.echo(f"Root directory: {root_dir}")
 
-        # ensure files exist
-        files = row["files"].split("|")
-        for file in files:
-            if not os.path.exists(file):
-                click.echo(f"Error: unable to find file at path {file}", err=True)
-                failure = True
+    with csvfile.open("r") as fh:
+        reader = csv.DictReader(fh)
+        for index, row in enumerate(reader):
+            if verbose:
+                click.echo(f"Processing row no. {index + 1}")
+            failure = False
 
-        if failure:
-            click.echo(f"Skipping row no. {index + 1} due to errors", err=True)
-            continue
+            # map filenames to paths & ensure they 1) exist & 2) are under root_dir (prevent path traversal)
+            files = map(
+                lambda f: (root_dir / Path(f)).resolve(), row["files"].split("|")
+            )
+            for file in files:
+                if not file.exists():
+                    click.echo(f"Error: unable to find file at path {file}", err=True)
+                    failure = True
+                if root_dir not in file.parents:
+                    click.echo(
+                        f"Error: {file} path points outside the root {root_dir} path",
+                        err=True,
+                    )
 
-        # create item, create file space, add files in parallel with httpx, publish item
-        # can model after Portal syllabus integration
+            if failure:
+                click.echo(f"Skipping row no. {index + 1} due to errors", err=True)
+                continue
+
+            # create item, create file space, add files in parallel with httpx, publish item
+            # can model after Portal syllabus integration
 
 
 if __name__ == "__main__":

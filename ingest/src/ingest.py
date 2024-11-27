94add166ea9bcdbc92f5154c1ef3b528eec3a5c0
phette23 <phette23@gmail.com>
2024-11-26 17:02:30 -0800

    feat: much work on ingest script
    creates items from the sheet in a particular collection now
    with a debug mode for just printing the XML metadata and file paths
    but the file uploads don't seem to work, they're uploaded as zero-byte
    files, have to work on that part

--- ingest/src/ingest.py
+++ ingest/src/ingest.py
@@ -1,7 +1,10 @@
 import csv
 from pathlib import Path
+from typing import Any
+import xml.etree.ElementTree as ET
 
 import click
+import httpx
 
 from coll import collections
 
@@ -20,9 +23,16 @@ APP_PREFIX = "INGEST"
 @click.option(
     "-c", "--collection", required=True, help="Name of collection to add items to"
 )
+@click.option("-d", "--debug", is_flag=True, help="Debug (do not create items)")
 @click.option("-t", "--token", required=True, help="VAULT access token")
 @click.option("-v", "--verbose", is_flag=True, help="Print diagnostic messages.")
-def main(csvfile: Path, collection="", token="", verbose=False):
+def main(
+    csvfile: Path,
+    collection: str = "",
+    debug: bool = False,
+    token: str = "",
+    verbose: bool = False,
+):
     collection_uuid: str | None = collections.get(collection, None)
     if not collection_uuid:
         click.echo(
@@ -30,7 +40,7 @@ def main(csvfile: Path, collection="", token="", verbose=False):
         )
         exit(1)
 
-    root_dir = csvfile.parent.resolve()
+    root_dir: Path = csvfile.parent.resolve()
     if verbose:
         click.echo(f"Root directory: {root_dir}")
 
@@ -39,11 +49,11 @@ def main(csvfile: Path, collection="", token="", verbose=False):
         for index, row in enumerate(reader):
             if verbose:
                 click.echo(f"Processing row no. {index + 1}")
-            failure = False
+            failure: bool = False
 
             # map filenames to paths & ensure they 1) exist & 2) are under root_dir (prevent path traversal)
-            files = map(
-                lambda f: (root_dir / Path(f)).resolve(), row["files"].split("|")
+            files: list[Path] = list(
+                map(lambda f: (root_dir / Path(f)).resolve(), row["files"].split("|"))
             )
             for file in files:
                 if not file.exists():
@@ -59,8 +69,88 @@ def main(csvfile: Path, collection="", token="", verbose=False):
                 click.echo(f"Skipping row no. {index + 1} due to errors", err=True)
                 continue
 
-            # create item, create file space, add files in parallel with httpx, publish item
-            # can model after Portal syllabus integration
+            # construct the XML metadata
+            metadata: ET.Element = ET.Element("xml")
+            local: ET.Element = ET.SubElement(metadata, "local")
+            wrapper: ET.Element = ET.SubElement(local, "courseInfo")
+            for field in ["semester", "section_code", "course_title"]:
+                element: ET.Element = ET.SubElement(wrapper, field.split("_")[0])
+                element.text = row[name]
+            department: ET.Element = ET.SubElement(wrapper, "department")
+            department.text = collection
+            mods: ET.Element = ET.SubElement(metadata, "mods")
+            name: ET.Element = ET.SubElement(mods, "name")
+            name_part: ET.Element = ET.SubElement(name, "namePart")
+            name_part.text = row["student"]
+            title_info: ET.Element = ET.SubElement(mods, "titleInfo")
+            title: ET.Element = ET.SubElement(title_info, "title")
+            title.text = " | ".join(
+                [row["semester"], row["section_code"], row["student"]]
+            )
+
+            if debug:
+                click.echo(f"Item no. {index} XML:")
+                click.echo(ET.tostring(metadata, encoding="unicode"))
+                click.echo(f"Item no. {index} attachments:")
+                for file in files:
+                    click.echo(f"- {file}")
+
+            else:
+                # create file area, add files (in parallel?), create item using filearea
+                # can model after Portal syllabus integration
+                # https://gitlab.com/california-college-of-the-arts/portal/-/blob/main/portal/apps/vault/syllabus.py
+                headers: httpx.Headers = httpx.Headers(
+                    {"X-Authorization": f"access_token={token}"}
+                )
+
+                # Obtain a file area for attachments
+                # ? should we use the /staging API route? /file is deprecated, apparently
+                response: httpx.Response = httpx.post(
+                    "https://vault.cca.edu/api/file", headers=headers
+                )
+                response.raise_for_status()
+                filearea: dict[str, str] = response.json()
+                # Upload files to the file area
+                # ? parallelize?
+                # ! uploaded files are zero bytes
+                # ? do we need to set content-length and content-type headers?
+                for file in files:
+                    response = httpx.put(
+                        f"https://vault.cca.edu/api/file/{filearea['uuid']}/content/{file.name}",
+                        files={"file": open(file, "rb")},
+                        headers=headers,
+                    )
+                    response.raise_for_status()
+
+                # Create an item using the file area
+                # Probably something to do with the structure of the item dict
+                item: dict[str, Any] = {
+                    "attachments": [],
+                    "collection": {"uuid": collection_uuid},
+                    "metadata": ET.tostring(metadata, encoding="unicode"),
+                }
+                for file in files:
+                    item["attachments"].append(
+                        {
+                            "description": file.name,
+                            "filename": file.name,
+                            "type": "file",
+                            # special value that'll be replaced by the attachment UUID
+                            "uuid": f"uuid:{file.name}",
+                        }
+                    )
+                # This is necessary or EQUELLA throws an Unsupported Media Type error
+                headers["Content-Type"] = "application/json"
+                response = httpx.post(
+                    f"https://vault.cca.edu/api/item?file={filearea['uuid']}",
+                    json=item,
+                    headers=headers,
+                )
+                response.raise_for_status()
+                # EQUELLA adds the new item's API URL in the Location header
+                click.echo(
+                    f"Created item: {response.headers['Location'].replace('/api/item/', '/items/')}"
+                )
 
 
 if __name__ == "__main__":
